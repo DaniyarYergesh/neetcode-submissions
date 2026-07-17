198180012ad917d369c8db279def3e624fd0f8d9
Daniyar Yergesh <86002170+DaniyarYergesh@users.noreply.github.com>
2026-07-17 13:10:33 +0500

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.kt
@@ -0,0 +1,25 @@
+class Solution {
+    fun trap(height: IntArray): Int {
+        val n = height.size
+        if(n == 0) return 0
+
+        var l = 0
+        var r = height.size - 1
+        var leftMax = height[l]
+        var rightMax = height[r]
+        var res = 0
+
+        while(l < r) {
+            if(leftMax < rightMax) {
+                l++
+                leftMax = maxOf(leftMax, height[l])
+                res += leftMax - height[l]
+            } else {
+                r--
+                rightMax = maxOf(rightMax, height[r])
+                res += rightMax - height[r]
+            }
+        }
+        return res
+    }
+}
